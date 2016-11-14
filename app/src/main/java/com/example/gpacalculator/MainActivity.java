1320b07c357a1d4221d0e0a03d3ef6fb9b492998
Rob Rose <bertra97@gmail.com>
2016-11-14 18:07:22 -0500

    Part 1 Start

--- app/src/main/java/com/example/gpacalculator/MainActivity.java
+++ app/src/main/java/com/example/gpacalculator/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.gpacalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView gpaTextView;
+    ListView classListView;
+    Button addClassButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: point to the correct views
+        gpaTextView = null;
+        classListView = null;
+        addClassButton = null;
     }
 }
