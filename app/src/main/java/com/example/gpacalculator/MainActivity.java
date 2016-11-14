cb854fa72f3816f06a507345afefecaa16d9216e
Rob Rose <bertra97@gmail.com>
2016-11-14 18:36:18 -0500

    Updated 1.1.2

--- app/src/main/java/com/example/gpacalculator/MainActivity.java
+++ app/src/main/java/com/example/gpacalculator/MainActivity.java
@@ -2,15 +2,23 @@ package com.example.gpacalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView gpaTextView;
-    ListView classListView;
+    ListView lectureListView;
     Button addClassButton;
+
+    ArrayAdapter<String> lectureListAdapter;
+
+    String[] exampleLecturesArray = {"Fencing", "Sailing", "Swiming", "Juggling"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: point to the correct views
         gpaTextView = (TextView) findViewById(R.id.gpa_text);
-        classListView = (ListView) findViewById(R.id.class_list);
+        classListView = (ListView) findViewById(R.id.lecture_list);
         addClassButton = (Button) findViewById(R.id.create_button);
+
+        ArrayList<String> exampleLecturesList  = (ArrayList<String>) Arrays.asList(exampleLecturesArray);
     }
 }
